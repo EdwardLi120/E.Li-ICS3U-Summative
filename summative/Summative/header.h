d76f73d1bb8224e0dc291a50e29ab727afcfc984
EdwardLi120 <eli19@ocdsb.ca>
2018-05-22 13:30:29 -0400

    Header File

---
+++ summative/Summative/header.h
@@ -0,0 +1,125 @@
+//header.h
+//#pragma once
+
+//globals
+
+const float FPS = 60;
+const int SCREEN_W = 900;
+const int SCREEN_H = 900;
+const int BOUNCER_SIZE = 32;
+const int NUM_BULLETS = 100;
+const int ROWS = 5;   //rows of enemies
+const int COLS = 3;  //columns of enemies
+const int frameCount = 33;
+const int MAX_ENEMY = 5; //max enemies on screen at one time
+const int ENEMY_COUNT = 20; //20 enemies
+const int SIZE = 1.5;  //3x3 square
+const int ABM_COUNT = 30;
+
+enum KEYS {
+	KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT, KEY_SPACE  //by default member 1 = 0, member 2 = 1, member 3 = 2, member 4 = 3...
+};
+
+enum ID { PLAYER, BULLET, ENEMY };
+
+enum BATTERY { LEFT, CENTER, RIGHT };
+
+
+//crosshair
+typedef struct crosshairData {
+	float x;
+	float y;
+	float height;
+	float width;
+	float target_x;
+	float target_y;
+} Crosshair;
+
+
+//each individual ABM has these properties
+typedef struct abmData {
+	int dest_x;
+	int dest_y;
+	int launch_x;
+	int launch_y;
+	float dx;
+	float dy;
+	float x_inc;
+	float y_inc;
+	float x_pos;
+	float y_pos;
+	float step;
+	int speed;
+	bool launched;
+	bool arrived;
+	bool exploded;
+	float explosionRadius;
+	bool increaseRadius;
+	int num_increment;
+}Abm;
+
+typedef struct enemyData {
+	int dest_x;
+	int dest_y;
+	int launch_x;
+	int launch_y;
+	float dx;
+	float dy;
+	float x_inc;
+	float y_inc;
+	float x_pos;
+	float y_pos;
+	float step;
+	int speed;
+	bool launched;
+	bool arrived;
+	bool exploded;
+	bool split;
+	int relativeX;
+	int relativeY;
+	int distX;
+	int distY;
+	int distTotal;
+	bool hit;
+
+	//bounds
+} Enemy;
+
+
+//multiple independently targetable reentry vehicle
+typedef struct mirvData {
+	Enemy branch[3];
+} Mirv;
+
+
+
+//prototypes
+int initAllegro(ALLEGRO_DISPLAY **display, ALLEGRO_TIMER **timer, ALLEGRO_BITMAP **imageCrosshair, ALLEGRO_EVENT_QUEUE **event_queue);
+void initCrosshair(Crosshair * crosshair, ALLEGRO_BITMAP * imageCrosshair);
+void initAbm(struct abmData * abm);
+
+void playerMovement(ALLEGRO_DISPLAY *display, ALLEGRO_TIMER *timer, ALLEGRO_BITMAP *imageCrosshair, ALLEGRO_EVENT_QUEUE *event_queue, Crosshair crosshair, struct abmData * abm, Enemy * enemy, int * curr_enemy_num, int * num_spawned, Mirv * mirv);
+
+void drawCrosshair(ALLEGRO_BITMAP *imageCrosshair, struct crosshairData data);
+
+void fire(Abm * abm, Crosshair crosshair);
+void calcIncrement(Abm * abm);
+void updateAbm(struct abmData * abm);
+void drawAbm(struct abmData * abm);
+void abmArrival(Abm * abm);  //check if abm arrived
+void drawExplosion(Abm * abm);
+
+void initEnemy(Enemy * enemy, Mirv * mirv);
+void spawnEnemy(Enemy * enemy, int * curr_enemy_count, int * num_spawned);
+void drawEnemy(Enemy * enemy);
+void updateEnemy(Enemy * enemy);
+void enemyArrival(Enemy * enemy, int * curr_num_enemy);
+
+void spawnMirv(Enemy * enemy, Mirv * mirv);
+void updateMirv(Mirv * mirv);
+void drawMirv(Mirv * mirv);
+void mirvArrival(Mirv * mirv, Enemy * enemy);
+
+void hitDetection(struct abmData * abm, Enemy * enemy);
+
+void print_arrive(Abm * abm, int *count);
